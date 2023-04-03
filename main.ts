4ad8404c3a4dfaa2833a9c833f6732eb8401f6b6
M. Allen Jeffrey <73103636+jeffzy15@users.noreply.github.com>
2023-04-03 18:44:43 +0800

    change radio and add appointment

--- main.ts
+++ main.ts
@@ -1,8 +1,13 @@
+enum RadioMessage {
+    Fall = 16412,
+    Response = 42258,
+    message1 = 49434
+}
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(steps)
 })
 input.onGesture(Gesture.SixG, function () {
-    radio.sendString("Fall")
+    radio.sendMessage(RadioMessage.Fall)
     basic.showLeds(`
         . # . # .
         . . . . .
@@ -14,16 +19,7 @@ input.onGesture(Gesture.SixG, function () {
     basic.clearScreen()
 })
 input.onButtonPressed(Button.AB, function () {
-    radio.sendString("Response")
-    basic.showLeds(`
-        . # . # .
-        . . . . .
-        # . . . #
-        . # . # .
-        . . # . .
-        `)
-    basic.pause(100)
-    basic.clearScreen()
+    appointment()
 })
 input.onButtonPressed(Button.B, function () {
     for (let index = 0; index < 30; index++) {
@@ -40,6 +36,59 @@ input.onButtonPressed(Button.B, function () {
     steps = 0
     parkinson = 0
 })
+input.onGesture(Gesture.Shake, function () {
+    radio.sendMessage(RadioMessage.Response)
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.clearScreen()
+})
+function appointment () {
+    basic.showString("month")
+    while (!(input.isGesture(Gesture.TiltLeft))) {
+        timeanddate.numericTime(function (hour, minute, second, month, day, year) {
+            if (input.isGesture(Gesture.TiltRight)) {
+                timeanddate.setDate(month + 1, day, 2023)
+                basic.showString(timeanddate.date(timeanddate.DateFormat.MD))
+            }
+        })
+    }
+    basic.showString("day")
+    while (!(input.isGesture(Gesture.TiltLeft))) {
+        timeanddate.numericTime(function (hour, minute, second, month, day, year) {
+            if (input.isGesture(Gesture.TiltRight)) {
+                timeanddate.setDate(month, day + 1, 2023)
+                basic.showString(timeanddate.date(timeanddate.DateFormat.MD))
+            }
+        })
+    }
+    basic.showString("hr")
+    while (!(input.isGesture(Gesture.TiltLeft))) {
+        timeanddate.numericTime(function (hour, minute, second, month, day, year) {
+            if (input.isGesture(Gesture.TiltRight)) {
+                hour = 6
+                timeanddate.set24HourTime(hour + 1, minute, 0)
+                basic.showString(timeanddate.time(timeanddate.TimeFormat.HMMAMPM))
+            }
+        })
+    }
+    basic.showString("min")
+    while (!(input.isGesture(Gesture.TiltLeft))) {
+        timeanddate.numericTime(function (hour, minute, second, month, day, year) {
+            if (input.isGesture(Gesture.TiltRight)) {
+                minute = 29
+                timeanddate.set24HourTime(hour, minute + 1, 0)
+                basic.showString(timeanddate.time(timeanddate.TimeFormat.HMMAMPM))
+            }
+        })
+    }
+    basic.showString(timeanddate.dateTime())
+}
 let parkinson = 0
 let steps = 0
 radio.setGroup(58)
@@ -49,4 +98,5 @@ basic.forever(function () {
         steps += 1
         radio.sendNumber(steps)
     }
+    radio.sendString(timeanddate.dateTime())
 })
